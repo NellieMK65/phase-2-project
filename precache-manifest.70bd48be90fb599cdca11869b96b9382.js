14e2d614e13c3810c63b2aab63e2813b6c4dd0da
NellieMK65 <nelsonmuriithi65@gmail.com>
2023-01-12 00:57:37 +0300

    Updates

---
+++ precache-manifest.70bd48be90fb599cdca11869b96b9382.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fea831989f9d144a662",
+    "url": "/phase-2-project/static/css/main.69d074ff.chunk.css"
+  },
+  {
+    "revision": "3fea831989f9d144a662",
+    "url": "/phase-2-project/static/js/main.0ff9c71c.chunk.js"
+  },
+  {
+    "revision": "acf85d303702cda76824",
+    "url": "/phase-2-project/static/js/runtime~main.58cae7f1.js"
+  },
+  {
+    "revision": "6737e7bd86d48750f750",
+    "url": "/phase-2-project/static/js/2.03b3570a.chunk.js"
+  },
+  {
+    "revision": "fe74e288e700074b85cd8484ff822dd9",
+    "url": "/phase-2-project/index.html"
+  }
+];
